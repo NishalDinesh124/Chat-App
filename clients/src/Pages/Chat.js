89a6d93800f1276e25273912b9e15a48dd79207c
NishalDinesh <nishaldinesh124@gmail.com>
2025-10-28 18:57:10 +0530

    Features:- New UI implemented, Improved mobile experience

--- clients/src/Pages/Chat.js
+++ clients/src/Pages/Chat.js
@@ -1,12 +1,11 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { allUsersRoute, host } from "../Utils/APIRoutes";
-import Contacts from "../Components/Contacts"
+import { allUsersRoute } from "../Utils/APIRoutes";
+import Contacts from "../Components/Contacts";
 import MessageContainer from "../Components/MessageContainer";
 import Welcome from "../Components/Welcome";
-
 
 export default function Chat() {
   const navigate = useNavigate();
@@ -14,70 +13,54 @@ export default function Chat() {
   const [currentChat, setCurrentChat] = useState(null);
   const [currentUser, setCurrentUser] = useState(undefined);
   const [selected, setSelected] = useState(false);
-  /////////////////// may be romoved
+
   useEffect(() => {
     const setVh = () => {
       const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
     };
-
-    setVh(); // initial set
-    window.addEventListener('resize', setVh); // update on resize
-
-    return () => {
-      window.removeEventListener('resize', setVh);
-    };
+    setVh();
+    window.addEventListener("resize", setVh);
+    return () => window.removeEventListener("resize", setVh);
   }, []);
 
   useEffect(() => {
-    getCurrentUser()
+    getCurrentUser();
   }, []);
 
   useEffect(() => {
-    getContacts();
+    if (currentUser) getContacts();
   }, [currentUser]);
+
   const getContacts = async () => {
     try {
-      if (currentUser) {
-        if (currentUser.isAvatarImageSet) {
-          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
-          setContacts(data.data);
-        } else {
-          navigate("/setAvatar");
-        }
+      if (currentUser.isAvatarImageSet) {
+        const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+        setContacts(data.data);
+      } else {
+        navigate("/setAvatar");
       }
     } catch (err) {
-      console.log("An error occured while fetching contacts", err);
-
+      console.log("An error occurred while fetching contacts", err);
     }
-
-  }
+  };
 
   const getCurrentUser = async () => {
-    if (!localStorage.getItem('chat-app-user')) {
+    const storedUser = localStorage.getItem("chat-app-user");
+    if (!storedUser) {
       navigate("/login");
     } else {
-      setCurrentUser(
-        await JSON.parse(
-          localStorage.getItem('chat-app-user')
-        )
-      );
+      setCurrentUser(JSON.parse(storedUser));
     }
-  }
-  const handleChatChange = (contact) => {
-
-    if (contact !== currentChat) {
-      setCurrentChat(contact);
-    }
-
   };
-  const handleHiddenTab = (select) => {
-    setSelected(select)
 
-  }
-  const handleBackFunction = (() => {
-    setSelected(false)
-  })
+  const handleChatChange = (contact) => {
+    if (contact !== currentChat) setCurrentChat(contact);
+  };
+
+  const handleHiddenTab = (select) => setSelected(select);
+  const handleBackFunction = () => setSelected(false);
+
   return (
     <Container>
       <div className="container">
@@ -92,12 +75,13 @@ export default function Chat() {
 
         {/* Right Section */}
         <div className="chat-area">
-          {/* Desktop: Show Welcome only if no chat */}
-          {!selected && !currentChat && (
-            <Welcome />
-          )}
+          {/* Show Welcome only on desktop and after currentUser loads */}
+          {currentUser &&
+            !selected &&
+            !currentChat &&
+            window.innerWidth > 720 && <Welcome />}
 
-          {/* Chat window visible only when a contact is selected */}
+          {/* Show messages only when contact selected */}
           {currentChat && selected && (
             <MessageContainer
               currentChat={currentChat}
@@ -111,40 +95,39 @@ export default function Chat() {
 }
 
 const Container = styled.div`
-height: calc(var(--vh, 1vh) * 100);
- width: 100vw;
- display: flex;
- flex-direction: column;
- justify-content: center;
- align-items: center;
-background: radial-gradient(circle at top left, #3b548dff, #374d70ff, #364964ff);
-
-.chat-area{
-  width: 100%;
-}
-
-.tab{
-  display: grid;
+  height: calc(var(--vh, 1vh) * 100);
+  width: 100vw;
+  display: flex;
   flex-direction: column;
-  overflow: auto;
+  justify-content: center;
+  align-items: center;
+  background: radial-gradient(circle at top left, #3b548dff, #374d70ff, #364964ff);
+
+  .chat-area {
+    width: 100%;
+  }
+
+  .tab {
+    display: grid;
+    flex-direction: column;
+    overflow: auto;
   }
 
   .container {
     height: 100vh;
     width: 100vw;
+    overflow: auto;
     display: grid;
     grid-template-columns: 25% 75%;
 
-    @media screen and (max-width: 720px){
-    grid-template-columns: 100%;
-    .tab{
-    border: none;
+    @media screen and (max-width: 720px) {
+      grid-template-columns: 100%;
+      .tab {
+        border: none;
+      }
+      .hidden-tab {
+        display: none;
+      }
     }
-    .hidden-tab{
-      display:none;
-}
-    }
-
-      
-}
+  }
 `;
